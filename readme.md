94c895a247ffeab483f2e5cdc53bd93fd77e57fd
ErikaFiatkoski <erika.fiatkoski06@gmail.com>
2024-11-10 20:20:51 -0300

    Alteracoes de aparencia e creditos

--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
-## Awesome JSGame Detona Ralph
+# :punch: Detona Ralph Game
 
 <p align="center">
-  <img src="https://github.com/digitalinnovationone/jsgame-detona-ralph/raw/main/assets/images/logo.png" alt="JSGame Detona Ralph Logo">
+  <img src="./src/images/Ralph_pose_transparent.png" alt="Detona Ralph Logo" width = 150px>
 </p>
 
-Bem-vindo ao **JSGame Detona Ralph**! Neste repositório, você encontrará um jogo divertido baseado no famoso filme "Detona Ralph". Este projeto não apenas oferece entretenimento, mas também demonstra várias técnicas avançadas de desenvolvimento de jogos em JavaScript.
+Bem-vindo ao **Detona Ralph Game**! Neste repositório, você encontrará um jogo divertido baseado no famoso filme "Detona Ralph".
 
 ### Tecnologias Utilizadas
 
@@ -14,14 +14,13 @@ Bem-vindo ao **JSGame Detona Ralph**! Neste repositório, você encontrará um j
 
 ### Funcionalidades Incríveis
 
-- **Sistema de Pontuação**: Acompanhe sua pontuação à medida que progride no jogo e desafie seus amigos a superá-la, clique no quadrado que o Ralph se encontre
+- **Sistema de Pontuação**: Acompanhe sua pontuação à medida que progride no jogo, basta clicar no quadrado em que Ralph se encontra.
 
 ### Como Jogar
 
 1. Clone este repositório para sua máquina local.
 2. Abra o arquivo `index.html` em seu navegador web.
-3. Use as setas direcionais para mover o personagem e a barra de espaço para interagir.
-4. Divirta-se explorando e coletando moedas, mas fique atento aos inimigos!
+3. Use o mouse para interagir, clicando nos quadrados.
 
 ### Contribuição
 
@@ -29,8 +28,9 @@ Contribuições são bem-vindas! Se você deseja melhorar este jogo, adicionar n
 
 ### Créditos
 
-Este jogo foi desenvolvido como parte de um projeto educacional da Digital Innovation One.
+- Este jogo foi desenvolvido como parte de um projeto educacional da Digital Innovation One.
+- O personagem apresentado no jogo pertence exclusivamente à Walt Disney, não sendo reservado à mim nenhum direito sobre ele.
 
 ---
 
-Divirta-se jogando o **JSGame Detona Ralph** enquanto explora as técnicas modernas de desenvolvimento de jogos em JavaScript. Lembre-se de conferir o repositório original [aqui](https://github.com/digitalinnovationone/jsgame-detona-ralph) e deixar uma ⭐️ se você gostou do projeto!
+Divirta-se jogando **Detona Ralph** enquanto explora as técnicas modernas de desenvolvimento de jogos em JavaScript. Lembre-se de conferir o repositório original [aqui](https://github.com/digitalinnovationone/jsgame-detona-ralph) e deixar uma ⭐️ se você gostou do projeto!
--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,65 +1,79 @@
+/*Variáveis usadas para alterar coisas visuais são guardadas no views.
+Variáveis que não alteram visualmente o projeto são guardadas no values.
+Variáveis que fazem ações por baixo dos panos são guardados no actions*/
+
 const state = {
   view: {
-    squares: document.querySelectorAll(".square"),
-    enemy: document.querySelector(".enemy"),
-    timeLeft: document.querySelector("#time-left"),
-    score: document.querySelector("#score"),
+      squares: document.querySelectorAll(".square"),
+      enemy: document.querySelector(".enemy"),
+      timeLeft: document.querySelector("#time-left"),
+      score: document.querySelector("#score"),
+      lives: document.querySelector("#lives"),
+
   },
   values: {
-    gameVelocity: 1000,
-    hitPosition: 0,
-    result: 0,
-    curretTime: 60,
+      gameVelocity: 1000,
+      hitPosition: 0,
+      result: 0,
+      currentTime: 15,
   },
   actions: {
-    timerId: setInterval(randomSquare, 1000),
-    countDownTimerId: setInterval(countDown, 1000),
-  },
-};
-
-function countDown() {
-  state.values.curretTime--;
-  state.view.timeLeft.textContent = state.values.curretTime;
-
-  if (state.values.curretTime <= 0) {
-    clearInterval(state.actions.countDownTimerId);
-    clearInterval(state.actions.timerId);
-    alert("Game Over! O seu resultado foi: " + state.values.result);
+      timerId: setInterval(randomSquare, 1000), //De tempo em tempo, move o inimigo para uma nova posição
+      countDownTimerId: setInterval(countDown, 1000),
   }
-}
+}; //Objeto criado para visualizar e manipular as variáveis presentes no projeto.
+
+function countDown(){
+  state.values.currentTime--;
+  state.view.timeLeft.textContent = state.values.currentTime;
+
+  if(state.values.currentTime <= 10){
+    state.view.timeLeft.classList.add("evidence");
+  }
+  if(state.values.currentTime <= 0){
+      clearInterval(state.actions.countDownTimerId)
+      clearInterval(state.actions.timerId)
+      state.view.timeLeft.classList.remove("evidence");
+      alert("Game Over! O seu resultado foi: " + state.values.result);
+  }
+}; //Verifica se o tempo acabou e mostra na tela qual a pontuação alcançada
+
+
+function randomSquare() {
+  state.view.squares.forEach((square) => {
+      square.classList.remove("enemy");
+  });
+
+let randomNumber = Math.floor(Math.random() * 9);
+
+let randomSquare = state.view.squares[randomNumber];
+randomSquare.classList.add("enemy");
+state.values.hitPosition = randomSquare.id;
+}; //Adiciona o inimigo em um square (quadrado) aleatório
 
 function playSound(audioName) {
   let audio = new Audio(`./src/audios/${audioName}.m4a`);
   audio.volume = 0.2;
   audio.play();
-}
+}; //Adiciona os arquivos de audio ao jogo, para serem chamados nas funções.
 
-function randomSquare() {
+
+function addListenerHitBox(){
   state.view.squares.forEach((square) => {
-    square.classList.remove("enemy");
-  });
+      square.addEventListener("mousedown", ()=> {
+          if(square.id === state.values.hitPosition){
+          state.values.result++
+          state.view.score.textContent = state.values.result;
+          state.values.hitPosition = null;
+          playSound("hit");
+          }
+      })
+  })
+}; //Verifica se o quadrado onde estamos clicando é o mesmo quadrado onde nosso inimigo se encontra.
 
-  let randomNumber = Math.floor(Math.random() * 9);
-  let randomSquare = state.view.squares[randomNumber];
-  randomSquare.classList.add("enemy");
-  state.values.hitPosition = randomSquare.id;
-}
-
-function addListenerHitBox() {
-  state.view.squares.forEach((square) => {
-    square.addEventListener("mousedown", () => {
-      if (square.id === state.values.hitPosition) {
-        state.values.result++;
-        state.view.score.textContent = state.values.result;
-        state.values.hitPosition = null;
-        playSound("hit");
-      }
-    });
-  });
-}
 
 function initialize() {
   addListenerHitBox();
-}
+}; //Função principal, que chama todas as outras.
 
 initialize();
